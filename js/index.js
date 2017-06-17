bd5a5baa2678b29ad6989980903d86f02fca2a1b
Alexander Moskovskiy <aleksandrmoskovskiy@a-mosk.local>
2017-06-18 02:51:09 +0300

    резиновый index

--- js/index.js
+++ js/index.js
@@ -1,5 +1,18 @@
 $(function() {
   $('#mi9').hover(function()
-  {$('#bi9, #mi1, #mi2, #mi3, #mi4, #mi5, #mi6, #mi7, #mi8, #mi10, #mi11, #mi12 ').toggleClass('hidden')}
+    {$('#bi9, #mi1, #mi2, #mi3, #mi4, #mi5, #mi6, #mi7, #mi8, #mi10, #mi11, #mi12 ').toggleClass('hidden')}
   )
+
+  $(window).resize(function() {
+    var documentWidth = document.documentElement.clientWidth
+    var transform = ((documentWidth / 1440)-0)
+
+    document.querySelector('section.museums').style.transform = 'scale(' + transform + ')';
+    // console.log(transform)
+  })
+
+  var documentWidth = document.documentElement.clientWidth
+  var transform = ((documentWidth / 1440)-0)
+
+  document.querySelector('section.museums').style.transform = 'scale(' + transform + ')';
 });
