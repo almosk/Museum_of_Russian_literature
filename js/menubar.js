0b2e5f71d989a737ddddee867eb75c34357f4633
Alexander Moskovskiy <aleksandrmoskovskiy@a-mosk.local>
2017-06-21 03:08:25 +0300

    менюха дан

--- js/menubar.js
+++ js/menubar.js
@@ -5,12 +5,22 @@ $(function() {
     var documentHeight = document.documentElement.clientHeight
     if ((-elTop) > documentHeight) {
       $('.menubar').removeClass('firstpage')
+			$('.arrow').css({"background-image": "url(img/arrow_black.svg)"});
     }
 
     if ((-elTop) < documentHeight) {
       $('.menubar').addClass('firstpage')
+			$('.arrow').css({"background-image": "url(img/arrow.svg)"});
     }
 
     // console.log('Scroll', elTop, documentHeight)
 	})
+
+	$('#ni1, #ni2').hover(function()
+    {$('.museumsList').toggleClass('hidden')}
+  )
+
+	$('#ni3').hover(function()
+		{$('.contentList').toggleClass('hidden')}
+  )
 })
